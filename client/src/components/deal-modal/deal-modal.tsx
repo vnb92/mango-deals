0edb19f0afcefa616de80f6b28c983b3c00c5f06
Vladimir Bogatyrev <v.bogatyrev@okko.tv>
2022-03-17 00:52:45 +0300

    feat: deal modal with mocks

--- client/src/components/deal-modal/deal-modal.tsx
+++ client/src/components/deal-modal/deal-modal.tsx
@@ -1,8 +1,21 @@
 import React, { FC } from "react"
 import { Modal, ModalProps } from "@/ui-kit/modal"
+import { Input } from "@/ui-kit/input"
+import { Button } from "@/ui-kit/button"
+import styles from './deal-modal.css'
 
 export const DealModal: FC<DealModalProps> = (props) => {
-  return <Modal {...props}>deal</Modal>
+  return (
+    <Modal {...props} title="Make a New Deal">
+      <div className={styles.content}>
+        <form className={styles.form}>
+          <Input label="Current Date" type="text" value="28 Jul 2021 15:58" disabled />
+          <Input label="Enter value" type="text" value={'299'} autoFocus />
+        </form>
+        <Button>Procced</Button>
+      </div>
+    </Modal>
+  )
 }
 
 export interface DealModalProps extends ModalProps {}
