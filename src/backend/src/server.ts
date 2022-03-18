8a4f39be481c3d32a825d567368b951f1ed7d134
Vladimir Bogatyrev <v.bogatyrev@okko.tv>
2022-03-19 02:41:26 +0300

    feat: add server pagination

--- src/backend/src/server.ts
+++ src/backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { v4 as uuid } from 'uuid';
+import paginate from 'express-paginate';
 
 const app = express();
 const port = 3000
@@ -9,9 +10,17 @@ console.log('Server was starting on port', port);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(paginate.middleware(10, 10));
 
-app.get('/api/deals', function(_, res) {
-  res.send(Deals);
+app.get('/api/deals', function(req, res) {
+  const { limit, page } = req.query as any
+  const dealsCount = Deals.length
+  const pageCount = Math.ceil(dealsCount / limit)
+  const leftHandDeals = page === 1 ? 0 : (page - 1) * limit
+  
+  const limitedDeals = Deals.slice(leftHandDeals, leftHandDeals+limit)  
+
+  res.send({ deals: limitedDeals, hasMore: paginate.hasNextPages(req)(pageCount) });
 });
 
 app.post('/api/deal', function(req, res) {
@@ -28,6 +37,16 @@ app.delete('/api/deal/:id', function(req, res) {
 });
 
 let Deals = [
-  { id: uuid(), date: new Date(), value: 100.1 },
-  { id: uuid(), date: new Date(), value: 50 },
+  { id: uuid(), date: new Date(), value: 1 },
+  { id: uuid(), date: new Date(), value: 2 },
+  { id: uuid(), date: new Date(), value: 3 },
+  { id: uuid(), date: new Date(), value: 4 },
+  { id: uuid(), date: new Date(), value: 5 },
+  { id: uuid(), date: new Date(), value: 6 },
+  { id: uuid(), date: new Date(), value: 7 },
+  { id: uuid(), date: new Date(), value: 8 },
+  { id: uuid(), date: new Date(), value: 9 },
+  { id: uuid(), date: new Date(), value: 10 },
+  { id: uuid(), date: new Date(), value: 11 },
+  { id: uuid(), date: new Date(), value: 12 },
 ]
